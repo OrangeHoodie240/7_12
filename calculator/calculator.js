c90429e5ed15970bfebbafff4a7b487c75286608
steve <daddarios@dupage.edu>
2021-01-23 00:27:20 -0600

    Made Calculator Functional

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,32 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  document.getElementById('loan-amount').value = 5000; 
+  document.getElementById('loan-years').value = 2; 
+  document.getElementById('loan-rate').value = .04; 
+
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const values = getCurrentUIValues(); 
+  let monthly = calculateMonthlyPayment(values);
+  updateMonthly(monthly);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let i = values.rate / 12; 
+  let n = 12 * values.years; 
+  return (Math.round(((values.amount * i)/(1 - (1+i)**(-n))) * 100) / 100) + '';
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  document.querySelector('#monthly-payment').innerHTML = `$${monthly}`;
 }
