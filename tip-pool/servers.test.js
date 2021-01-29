1e4607e558b2d44600d254eba26c741fffe372b5
steve <daddarios@dupage.edu>
2021-01-29 03:15:46 -0600

    Cleaned up DOM after servers.test.js and added test for updateServerTable()

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -11,7 +11,28 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
+  it("Should create single row element on updateServerTable()", function(){
+
+    // set up code from submitServerInfo()
+    let serverName = serverNameInput.value;
+
+    if (serverName !== '') {
+      serverId++;
+      allServers['server' + serverId] = { serverName };
+  
+      updateServerTable();
+  
+      serverNameInput.value = '';
+    }
+
+    expect(serverTbody.children.length).toEqual(1); 
+    expect(serverTbody.children[0].tagName).toEqual('TR'); 
+
+  });
+
   afterEach(function() {
     // teardown logic
+    allServers = {}; 
+    serverTbody.innerHTML = '';
   });
 });
